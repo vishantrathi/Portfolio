70d00c391466129f919171e849ac77d3cfc3b9e2
vishantrathi <vishantrathigurjar@gmail.com>
2024-11-17 15:55:21 +0530

    add script to block inspect

---
+++ script.js
@@ -0,0 +1,18 @@
+//block inspect
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+document.onkeydown = function(e) {
+    if (e.keyCode == 123) { // F12 key
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { // Ctrl+Shift+J
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { // Ctrl+U
+        return false;
+    }
+};
